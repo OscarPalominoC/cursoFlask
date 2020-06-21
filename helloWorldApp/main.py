5575b9547abf26034d295e303b1da36330d1dfc0
Oscar Palomino <ing.oscarp1@gmail.com>
2020-06-20 21:43:48 -0500

    Configuracion de flask y uso d llaves secretas

--- helloWorldApp/main.py
+++ helloWorldApp/main.py
@@ -1,24 +1,32 @@
-from flask import Flask, request, make_response, redirect, render_template, abort
+from flask import Flask, request, make_response, redirect, render_template, abort, session
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+TODOS = ['To do 1', 'To do 2', 'To do 3']
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     return render_template('hello.html', user_ip = user_ip)
 
-TODOS = ['To do 1', 'To do 2', 'To do 3']
 
 @app.route('/control-structure')
 def control():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip' : user_ip, 
         'todos' : TODOS
@@ -26,9 +34,21 @@ def control():
     return render_template('control-structure.html', **context)
 
 
+@app.route('/test-bootstrap')
+def testBootstrap():
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    context = {
+        'user_ip' : user_ip, 
+        'todos' : TODOS
+    }
+    return render_template('test-bootstrap.html', **context)
+
+
 @app.route('/templates-inheritance')
 def templates():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip' : user_ip, 
         'todos' : TODOS
@@ -38,7 +58,8 @@ def templates():
 
 @app.route('/static')
 def statics():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip' : user_ip, 
         'todos' : TODOS
